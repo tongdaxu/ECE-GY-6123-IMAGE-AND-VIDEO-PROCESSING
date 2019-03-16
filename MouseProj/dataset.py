8e093602f1694bf951e2dcc144f4d4b3cace84e0
matonglidewazi <matonglidewazi@qq.com>
2019-03-16 13:36:03 -0400

    move transform, downsample as transform

--- MouseProj/dataset.py
+++ MouseProj/dataset.py
@@ -1,48 +1,125 @@
 import torch
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-from scipy.ndimage import rotate
+from torch.utils.data import Dataset
+from scipy.ndimage import affine_transform
 
-def Rotate3D (img, x, y, z, order):
+def toTensor (sample, device):
 	'''
+	Notes:
+		Convert sample to single tensor and move to device
 	Args:
-		image: ndarray (1, H, W, D)
-		flu: upper bound of degree of rotation
+		sample: dict of tensor, image in [0, 255], (N, C=1, X, Y, Z), 
+			label in {0, 0.5, 1}, (N, C=1, X, Y, Z)
+		device: torch.device('cuda')/torch.device('cpu')
 	Ret:
-		rotated image
+		imageTensor in [0, 1], (N, C=1, X, Y, X)
+		labelTensor in [0, 1], (N, C=3, X, Y, X)
 	'''
+	image, label = sample['image'], sample['label']
+	imageTensor = torch.from_numpy(image/255)
+	imageTensor = imageTensor.to(device=device)
 
-	img[0] = rotate(img[0], x, axes=(1, 0), reshape=False, order=order)
-	img[0] = rotate(img[0], y, axes=(1, 2), reshape=False, order=order)
-	img[0] = rotate(img[0], z, axes=(0, 2), reshape=False, order=order)
+	labelOH = np.zeros((image.shape[0], 3, image.shape[2], image.shape[3], image.shape[4]) \
+		, dtype=np.float32)
+
+	labelOH[:, 0:1] = (label < 0.33).astype(np.float32)
+	labelOH[:, 1:2] = (label > 0.33).astype(np.float32)
+	labelOH[:, 2:3] = (label > 0.66).astype(np.float32)
+
+	labelTensor = torch.from_numpy(labelOH)
+	labelTensor = labelTensor.to(device=device)
+
+	return imageTensor, labelTensor
+
+
+def AffineFun(img, xr, yr, zr, xm, ym, zm, order):
+	'''
+	Notes:
+		Rotate and move
+	Args:
+		img: image of shape (C=1, X, Y, Z)
+		xr, yr, zr: Rotate in degree
+		xm, ym, zm: move as int
+		order: 3 for image, 0 for label
+	Ret:
+		img: Transformed image of shape (C=1, X, Y, Z)
+	'''
+	sinx = np.sin(np.deg2rad(xr))
+	cosx = np.cos(np.deg2rad(xr))
+
+	siny = np.sin(np.deg2rad(yr))
+	cosy = np.cos(np.deg2rad(yr))
+
+	sinz = np.sin(np.deg2rad(zr))
+	cosz = np.cos(np.deg2rad(zr))
+
+	xc = img[0].shape[0]//2
+	yc = img[0].shape[1]//2
+	zc = img[0].shape[2]//2
+
+	Mc = np.array([[1, 0, 0, xc],[0, 1, 0, yc],[0, 0, 1, zc],[0, 0, 0, 1]])
+	Rx = np.array([[cosx, sinx, 0, 1],[-sinx, cosx, 0, 1],[0, 0, 1, 1], [0, 0, 0, 1]])
+	Ry = np.array([[cosy, 0, siny, 1],[0, 1, 0, 1],[-siny, 0, cosy, 1], [0, 0, 0, 1]])
+	Rz = np.array([[1, 0, 0, 1],[0, cosz, sinz, 1],[0, -sinz, cosz, 1], [0, 0, 0, 1]])
+	Mb = np.array([[1, 0, 0, -xc],[0, 1, 0, -yc],[0, 0, 1, -zc],[0, 0, 0, 1]])
+	MM = np.array([[1, 0, 0, xm],[0, 1, 0, ym],[0, 0, 1, zm],[0 ,0, 0, 1]])
+
+	Matrix = np.linalg.multi_dot([Mc, Rx, Ry, Rz, Mb, MM])
+	img[0] = affine_transform(img[0], Matrix, output_shape=img[0].shape, order=order)
 
 	return img
 
-class RandomRotate(object):
+def downSampleFun(img, level, order):
 	'''
-	Random rotation
+	Args:
+		img: shape [1, X, Y, Z]
+		level: scaling factor of downsampling
+		order: 3 for image, 0 for label
+	Ret:
+		imgout: down sampled image of shape [1, X//level, Y//level, Z//level]
 	'''
-	def __init__(self, flu):
-		self.flu = flu
+	if level == 1:
+		return img
+	else:
+		_, x, y, z = img.shape
+
+		imgout = np.zeros([1, x//level, y//level, z//level], dtype=np.float32)
+		Matrix = np.array([[level, 0, 0, 0],[0, level, 0, 0],[0, 0, level, 0],[0, 0, 0, 1]])
+		imgout[0] = affine_transform(img[0], Matrix, output_shape=imgout[0].shape, order=order)
+		return imgout
+
+class downSample(object):
+	'''
+	Down sample happens before affine
+	'''
+	def __init__(self, level):
+
+		self.level = level
 		pass
 
 	def __call__(self, sample):
-		x = np.random.uniform(-self.flu, self.flu)
-		y = np.random.uniform(-self.flu, self.flu)
-		z = np.random.uniform(-self.flu, self.flu)
+
 		image, label = sample['image'], sample['label']
-		return {'image': Rotate3D(image, x, y, z, 3), 'label': Rotate3D(label, x, y, z, 0)}
+		return {'image': downSampleFun(image, self.level, 3), \
+				'label': downSampleFun(label, self.level, 0)}
 
-class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
+class RandomAffine(object):
+	'''
+	Random rotation and move
+	'''
+	def __init__(self, fluR, fluM):
 
-    def __call__(self, sample):
-        image, label = sample['image'], sample['label']
+		self.fluR = fluR
+		self.fluM = fluM
 
+	def __call__(self, sample):
 
-        return {'image': torch.from_numpy(image),
-                'label': torch.from_numpy(label)}
+		xr, yr, zr = np.random.uniform(-self.fluR, self.fluR, size=3)		
+		xm, ym, zm = np.random.uniform(-self.fluM, self.fluM, size=3)
+
+		image, label = sample['image'], sample['label']
+		return {'image': AffineFun(image, xr, yr, zr, xm, ym, zm, 3), \
+				'label': AffineFun(label, xr, yr, zr, xm, ym, zm, 0)}
 
 class niiDataset(Dataset):
 	'''
@@ -72,7 +149,7 @@ class niiDataset(Dataset):
 	def __getitem__(self, index):
 		'''
 		Override: integer indexing in range from 0 to len(self) exclusive.
-		type: converting to float tensor
+		type: keep as np array
 		'''
 		sample = {'image':np.copy(self.image[index]), 'label':np.copy(self.label[index])}
 
--- MouseProj/loss.py
+++ MouseProj/loss.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 
 def dice_loss(input, target):
 	'''
@@ -7,28 +6,18 @@ def dice_loss(input, target):
 	All conversion happens explicitly in the train function
 	Args: 
 		input: tensor of shape (N, 3, H, W, D), dtype = float
-		target: ndarray of shape (N, 1, H, W, D) dtype = float, not hotcoded yet
+		target: ndarray of shape (N, 3, H, W, D) dtype = float
 	Ret:
-		loss:
+		loss: 1 - DiceCoefficient
 	'''
-	N = input.size()[0]
 	C = input.size()[1]
-
-	targetOH = np.zeros(input.shape, dtype=np.float32)
-
-	targetOH[:, 0:1] = (target < 0.33).astype(np.float32)
-	targetOH[:, 1:2] = (target > 0.33).astype(np.float32)
-	targetOH[:, 2:3] = (target > 0.66).astype(np.float32)
-
-	targetTensor = torch.from_numpy(targetOH)
-
-	assert input.size() == targetTensor.size() # make sure input and target has same size
+	assert input.size() == target.size() # make sure input and target has same size
 	
 	eplison = 1e-6
 	loss = 0
 
 	for i in range (C):
-		loss += torch.sum(2*input.narrow(1, i, 1)*targetTensor.narrow(1, i, 1))/ \
-			(torch.sum(input.narrow(1, i, 1)) + torch.sum(targetTensor.narrow(1, i, 1)) + eplison)
+		loss += torch.sum(2*input.narrow(1, i, 1)*target.narrow(1, i, 1))/ \
+			(torch.sum(input.narrow(1, i, 1)) + torch.sum(target.narrow(1, i, 1)) + eplison)
 
-	return 1-loss/(N*C)
+	return 1-loss/(C)
